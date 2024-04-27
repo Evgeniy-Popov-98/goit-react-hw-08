eba771c0bb95b7f9c1a501cceb982525312cacf5
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-04-27 15:42:00 +0300

    Update LoginPage.jsx

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,15 @@
 import LoginForm from "../../components/LoginForm/LoginForm";
+import { useSelector } from "react-redux";
+import { apiRegister } from "../../redux/auth/operations";
 
 // import clsx from "clsx";
 // import style from "./RegisterPage.module.css";
 
 const LoginPage = () => {
+  //   const test = useSelector((state) => state.contacts.loading);
+  const test = apiRegister();
+
+  console.log(test);
   return (
     <div>
       <h2>Hello</h2>
